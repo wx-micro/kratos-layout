2baabb1ea9edf918041ee6dd1fb915f515d24715
ainiok <jx_xiaojin@aliyun.com>
2023-08-22 11:49:51 +0800

    init layout

---
+++ cmd/server/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/zero-one-cloud/shop-layout/internal/biz"
+	"github.com/zero-one-cloud/shop-layout/internal/conf"
+	"github.com/zero-one-cloud/shop-layout/internal/data"
+	"github.com/zero-one-cloud/shop-layout/internal/server"
+	"github.com/zero-one-cloud/shop-layout/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Env, *conf.Server, *conf.Data, *conf.Bootstrap, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
